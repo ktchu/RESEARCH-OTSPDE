b1e8adfd221804a6e907951a5bab03f8c8e12983
ktchu <none@none>
2008-11-15 20:29:30 +0000

    OTSPDE: - updated getFigures.sh script.

--- papers/OTSPDE/getFigures.sh
+++ papers/OTSPDE/getFigures.sh
@@ -10,14 +10,12 @@
 FIGURES_DIR="figures"
 
 SRC_DIR="../../src"
-EXAMPLE_DIR_LIST="1d/advection_diffusion_eqn                            \
-                  1d/burgers_eqn/single_hump                            \
-                  1d/diffusion_eqn                                      \
-                  1d/fourth_order_parabolic                             \
-                  2d/advection_eqn                                      \
-                  2d/diffusion_eqn/square_domain                        \
-                  2d/diffusion_eqn/irregular_domain/circle_domain       \
-                  2d/diffusion_eqn/irregular_domain/starfish_domain     \
+EXAMPLE_DIR_LIST="scalar_PDEs/1d/burgers_eqn/single_hump                       \
+                  scalar_PDEs/1d/diffusion_eqn                                 \
+                  scalar_PDEs/1d/fourth_order_parabolic                        \
+                  scalar_PDEs/2d/advection_eqn                                 \
+                  scalar_PDEs/2d/diffusion_eqn/square_domain                   \
+                  scalar_PDEs/2d/diffusion_eqn/irregular_domain/starfish_domain\
                  "
 
 # create directory for figures if it doesn't already exist
