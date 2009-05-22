b260dcf8bc6247a2ba682e3dfc14cad464cff48f
ktchu <none@none>
2009-05-22 04:21:16 +0000

    OTSPDE: - renamed 1d diffusion equation examples so that '1d' goes with 'DiffusionEqn'   instead of numerical method.

--- src/scalar_PDEs/1d/diffusion_eqn/solveDiffusionEqn1dCrankNicholson.m
+++ src/scalar_PDEs/1d/diffusion_eqn/solveDiffusionEqn1dCrankNicholson.m
@@ -1,30 +1,30 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% solveDiffusionEqnForwardEuler1d() computes the solutions of the 1d 
-% diffusion equation 
+% solveDiffusionEqn1dCrankNicholson() computes the solutions of the 1d
+% diffusion equation
 %
 %   u_t = D u_xx + f(x)
 %
 % on the domain 0 < x < 1 with a Dirichlet boundary condition imposed at
 % x = 0 and a Neumann boundary condition imposed at x = 1.  The numerical
-% solution is computed on a node-centered grid using forward Euler time 
-% integration with a second-order central difference approximation to the 
-% Laplacian.  
+% solution is computed on a node-centered grid using Crank-Nicholson time
+% integration using the standard second-order discretization of the 
+% Laplacian.  A suboptimal time step is used.
 %
 % USAGE:
 %   function [u, u_exact, x, timing_data] = ...
-%     solveDiffusionEqnForwardEuler1d(D, ...
-%                                     source_term_type, ...
-%                                     u_0, dudx_1, ...
-%                                     dx, dt, ...
-%                                     t_init, t_final, ...
-%                                     debug_on, timing_on)
+%     solveDiffusionEqn1dCrankNicholson(D, ...
+%                                       source_term_type, ...
+%                                       u_0, dudx_1, ...
+%                                       dx, dt, ...
+%                                       t_init, t_final, ...
+%                                       debug_on, timing_on)
 %
 % Arguments:
-% - D:                   diffusion coefficient 
+% - D:                   diffusion coefficient
 % - source_term_type:    type of source term to use in computation.
 %                        0:  f = 0
-%                        1:  
+%                        1:
 %
 %           f = 5*sin(3/2*pi*x) - 7*sin(15/2*pi*x) + 10*sin(21/2*pi*x)
 %
@@ -53,7 +53,7 @@
 %
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
+% 
 % CHANGE LOG:
 % -----------
 % 2007/09:  Initial version of code. 
@@ -61,23 +61,23 @@
 %
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
+% 
 % Kevin T. Chu
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [u, u_exact, x, timing_data] = ...
-  solveDiffusionEqnForwardEuler1d(D, ...
-                                  source_term_type, ...
-                                  u_0, dudx_1, ...
-                                  dx, dt, ...
-                                  t_init, t_final, ...
-                                  debug_on, timing_on)
+  solveDiffusionEqn1dCrankNicholson(D, ...
+                                    source_term_type, ...
+                                    u_0, dudx_1, ...
+                                    dx, dt, ...
+                                    t_init, t_final, ...
+                                    debug_on, timing_on)
 
 
 % check arguments
 if (nargin < 8)
-  error('solveDiffusionEqnForwardEuler1d: missing arguments');
+  error('solveDiffusionEqn1dCrankNicholson: missing arguments');
 end
 if (nargin < 9)
   debug_on = 0;
@@ -88,14 +88,16 @@ end
 
 % construct grid
 N = 1/dx + 1;
-x = 0:dx:1+dx;  x = x';   % include ghost cell for Neumann BC
+x = dx:dx:1;  x = x';   % exclude Dirichlet boundary
+x_plot = [0; x];
 
-% construct Laplacian operator (with boundary conditions) 
-e = ones(N+1,1);                         
-L = 1/dx^2*spdiags([e -2*e e], -1:1, N+1, N+1);
-L(1,1) = 0; L(1,2) = 0;   % no need to compute Laplacian at Dirichlet boundary 
-L(N+1,N) = 0; L(N+1,N+1) = 0;  % no need to compute Laplacian for 
-                               % ghost cell at Neumann boundary
+% construct Laplacian operators (with boundary conditions) 
+e = ones(N,1);                         
+L = 1/dx^2*spdiags([e -2*e e], -1:1, N-1, N-1);
+L(N-1,N-2) = 2/dx/dx; L(N-1,N-1) = -2/dx/dx; % modified for Neumann BC at x = 1
+
+L_rhs = speye(N-1) + D*dt/2*L;
+L_lhs = speye(N-1) - D*dt/2*L;
 
 % start clock to measure computation 
 if (timing_on == 1)
@@ -105,23 +107,23 @@ end
 % set initial conditions
 u = 1.0 + 0.5*x + 2*sin(5/2*pi*x) - 4*sin(11/2*pi*x) + 3*sin(7/2*pi*x);
 
-% set source term 
+% set source term
 if (source_term_type == 0)
   f = zeros(size(x));
 elseif (source_term_type == 1)
   f = 5*sin(3/2*pi*x) - 7*sin(15/2*pi*x) + 10*sin(21/2*pi*x);
 else
-  error('solveDiffusionEqnForwardEuler1d: Invalid source term type.  Valid valuesare 0 and 1');
+  error('solveDiffusionEqnForwardEuler1d: Invalid source term type. Valid valuesare 0 and 1');
 end
 
-% forward Euler time integration
+% Crank-Nicholson time integration
 t = t_init;
 while (t < t_final)
 
   if (debug_on == 1)
 
     % compute exact solution and err
-    if (source_term_type == 0)
+    if (f == 0)
       u_exact = 1.0 + 0.5*x ...
               + 2*sin(5/2*pi*x)*exp(-25/4*pi^2*D*t) ...
               - 4*sin(11/2*pi*x)*exp(-121/4*pi^2*D*t) ...
@@ -135,20 +137,20 @@ while (t < t_final)
               - 7/D*4/225/pi^2*sin(15/2*pi*x)*(1-exp(-225/4*pi^2*D*t)) ...
               + 10/D*4/21^2/pi^2*sin(21/2*pi*x)*(1-exp(-21^2/4*pi^2*D*t));
     end
-    err = u(1:N)-u_exact(1:N);
+    err = u-u_exact;
     err_L_inf = norm(err,'inf')
 
     % plot current solution
     figure(1); clf;
-    plot(x(1:N),u(1:N),'bo');
+    plot(x_plot,[u_0; u],'bo');
     hold on;
-    plot(x(1:N),u_exact(1:N),'r');
+    plot(x_plot,[u_0; u_exact],'r');
     title_string = sprintf('t = %f',t);
     title(title_string);
 
     % plot current error
     figure(2); clf;
-    plot(x(1:N),err);
+    plot(x,err);
     title_string = sprintf('t = %f',t);
     title(title_string);
     drawnow
@@ -158,14 +160,17 @@ while (t < t_final)
   % adjust dt so we don't overstep t_final
   if (t+dt > t_final)
     dt = t_final - t; 
+    L_rhs = eye(N-1) + D*dt/2*L;
+    L_lhs = eye(N-1) - D*dt/2*L;
   end
 
-  % impose boundary conditions
-  u(1) = u_0;
-  u(N+1) = u(N-1) + 2*dx*dudx_1;
+  % compute RHS 
+  rhs = L_rhs*u + dt*f;
+  rhs(1) = rhs(1) + D*dt/dx/dx*u_0;      % Dirichlet BC at x = 0
+  rhs(N-1) = rhs(N-1) + 2*D*dt/dx*dudx_1;  % Neumann BC at x = 1
 
   % update solution
-  u = u + dt*D*(L*u) + dt*f;
+  u = L_lhs\rhs;
 
   % update time
   t = t + dt;
@@ -187,7 +192,7 @@ if (timing_on)
     disp(' ');
     disp('==================================================================');
     disp('Computation Statistics');
-    comp_t_solve = sprintf('  Solution Time: %f', t_solve);
+    comp_time_str = sprintf('  Solution Time: %f', t_solve);
     disp(comp_time_str);
     disp('==================================================================');
   end
@@ -198,13 +203,13 @@ else
 
 end
 
-% compute exact solution 
+% compute exact solution
 if (source_term_type == 0)
   u_exact = 1.0 + 0.5*x ...
           + 2*sin(5/2*pi*x)*exp(-25/4*pi^2*D*t) ...
           - 4*sin(11/2*pi*x)*exp(-121/4*pi^2*D*t) ...
           + 3*sin(7/2*pi*x)*exp(-49/4*pi^2*D*t);
-else
+else      
   u_exact = 1.0 + 0.5*x ...
           + 2*sin(5/2*pi*x)*exp(-25/4*pi^2*D*t) ...
           - 4*sin(11/2*pi*x)*exp(-121/4*pi^2*D*t) ...
@@ -214,7 +219,7 @@ else
           + 10/D*4/21^2/pi^2*sin(21/2*pi*x)*(1-exp(-21^2/4*pi^2*D*t));
 end
 
-% remove ghost cell
-u = u(1:N);
-u_exact = u_exact(1:N);
-x = x(1:N);
+% add back Dirichlet boundary condition
+u = [u_0; u];
+u_exact = [u_0; u_exact];
+x = [0; x];
